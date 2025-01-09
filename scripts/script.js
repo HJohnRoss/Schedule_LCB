402560939951d1135202288e81fc9f6aa630af6c
John Ross <rossjohn936@gmail.com>
2025-01-09 08:45:35 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,19 @@
 const container = document.getElementById("container")
 function updateText() {
-    console.log(container.attributes.id)    
-       console.log(container.attributes.id)    
     while(container.firstChild) {
         container.firstChild.remove();
     }
     // if you make a new file change below to the link
     fetch("https://hjohnross.github.io/Schedule.txt_LCB/schedule.txt")
-        .then(response => response.text())
+        .then(res => res.text())
         .then(res => {
+            console.log(res)
             const lines = res.split('\n');
             for(i = 0; i < lines.length; i++) {
                 if(i === 0) {
-                    container.innerHTML += `<h1>${lines[i]}</h1>`
+                    container.innerHTML += `<h1 style="white-space:pre-wrap;">${lines[i]}</h1>`
                 } else {
-                    container.innerHTML += `<p>${lines[i]}</p>`
+                    container.innerHTML += `<p style="white-space:pre-wrap;">${lines[i]}</p>`
                 }
             }
         })
@@ -26,7 +25,6 @@ function removeId() {
     } else {
         container.removeAttribute("id")
     }
-    console.log("test")
 }
 updateText();
 setInterval(updateText, 120000)
